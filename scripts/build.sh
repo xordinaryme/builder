d4b62cf0859fb6e6db37768dd5ee7a661a2c9ccf
Liam <belowzeroiq@proton.me>
2025-03-25 00:54:32 +0700

    Disable OTA enforcement for VINTF kernel requirements in build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 build() {
 source build/envsetup.sh || . build/envsetup.sh
-bash device/xiaomi/fleur/prebuilts/vendor.sh
+#bash device/xiaomi/fleur/prebuilts/vendor.sh
 lunch $MAKEFILENAME-$VARIENT
 export SKIP_ABI_CHECKS=true
 export SKIP_API_CHECKS=true
 export ALLOW_MISSING_DEPENDENCIES=true
+export PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS=false
 $EXTRACMD
 $TARGET -j$(nproc --all) & sleep 95m || curl --upload-file ./out/error.log https://free.keep.sh > link.txt && cat link.txt
 }
