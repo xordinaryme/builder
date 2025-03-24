c6206156dff7f9fb9c29e5a40183059fe1f61ad0
Liam <belowzeroiq@proton.me>
2025-03-25 01:05:05 +0700

    Fix clone command in sync script to specify branch for Xiaomi hardware repository

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -11,7 +11,7 @@ sync() {
 repo init --depth=1 --no-repo-verify -u $ROMREPO
 git clone $LOCALMANIFEST .repo/local_manifests
 repo sync --current-branch --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$( nproc --all )
-git clone https://github.com/LineageOS/android_hardware_xiaomi.git b lineage-20 hardware/xiaomi
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-20 hardware/xiaomi
 }
 echo "Syncing Rom & Device Sources"
 sync
