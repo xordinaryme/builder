a768426332ed079a1284edc484c2a77dd1ca18ff
Liam <belowzeroiq@proton.me>
2025-03-25 02:08:23 +0700

    Update setup script to install essential packages for AOSP build environment

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 setup() {
-sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y
+sudo apt update && sudo apt install pigz wget jq curl repo -y
 sudo apt upgrade -y
 }
 echo "Setting Up AOSP Build Environment"
