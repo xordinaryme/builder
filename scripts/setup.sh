35722a9b149203bd0218c3b2797b9a4cb04afa24
Liam <belowzeroiq@proton.me>
2025-03-25 02:04:09 +0700

    Update setup script to install necessary packages for AOSP build environment and remove ccache download function

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,16 +1,7 @@
 #!/bin/bash
 setup() {
-sudo apt update &&sudo apt install pigz wget jq curl repo -y
+sudo apt install bc bison build-essential ccache curl flex g++-multilib gcc-multilib git git-lfs gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev lib32z1-dev liblz4-tool libncurses5 libncurses5-dev libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev -y
 sudo apt upgrade -y
 }
 echo "Setting Up AOSP Build Environment"
 setup
-
-ccache() {
-cd /tmp
-time aria2c $LINK -x16 -s50 || echo "No ccache link provided, build will fail due to time limit"
-time tar xf ccache.tar.gz
-cd ~/
-}
-echo "Downloading CCACHE"
-ccache
