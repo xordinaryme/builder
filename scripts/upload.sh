b070e799e6d3fb4f0a0dae2d615bb5cf0e38bf71
Liam <belowzeroiq@proton.me>
2025-03-25 02:05:02 +0700

    Refactor upload script to remove ccache handling and update ROM upload filename pattern

--- scripts/upload.sh
+++ scripts/upload.sh
@@ -1,28 +1,8 @@
 #!/bin/bash
-rccache() {
-cd /tmp
-rm ccache.tar.gz
-}
-echo "Removing Old CCACHE"
-rccache
-
-compress() {
-time tar --use-compress-program="pigz -k -$2 " -cf $1.tar.gz $1 ccache 1
-}
-echo "Compressing New CCACHE"
-compress
-
-uccache() {
-mkdir -p ~/.config/rclone
-echo "$E" > ~/.config/rclone/rclone.conf
-time rclone copy ccache.tar.gz $F -P
-}
-echo "Uploading CCACHE"
-uccache
 
 rom_upload() {
 cd ~/
-curl --upload-file ./out/target/product/$DEVICE/Sup*.zip https://free.keep.sh > link.txt && cat link.txt
+curl --upload-file ./out/target/product/$DEVICE/lineage*.zip https://free.keep.sh > link.txt && cat link.txt
 curl --upload-file ./out/target/product/$DEVICE/boot.img https://free.keep.sh > link1.txt && cat link1.txt
 }
 echo "Uploading Rom & Boot From /Out"
