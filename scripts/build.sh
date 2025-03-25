663d509bf72928acc44bb1b7652916cf27755181
Liam <belowzeroiq@proton.me>
2025-03-25 23:02:19 +0700

    Remove commented-out export variables from build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,12 +2,12 @@
 build() {
 source build/envsetup.sh || . build/envsetup.sh
 lunch $MAKEFILENAME-$VARIENT
-export SKIP_ABI_CHECKS=true
-export SKIP_API_CHECKS=true
-export ALLOW_MISSING_DEPENDENCIES=true
-export PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS=false
+#export SKIP_ABI_CHECKS=true
+#export SKIP_API_CHECKS=true
+#export ALLOW_MISSING_DEPENDENCIES=true
+#export PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS=false
 $EXTRACMD
-$TARGET -j$(nproc --all) & sleep 95m || curl --upload-file ./out/error.log https://free.keep.sh > link.txt && cat link.txt
+$TARGET -j$(nproc --all) & sleep 95m
 }
 echo "Initializing Build System"
 build
