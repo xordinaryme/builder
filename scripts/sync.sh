c8b426f58f039bf35fe27b9393e07f51c7945273
Liam <belowzeroiq@proton.me>
2025-03-25 23:01:10 +0700

    Remove unnecessary hardware directory cleanup from sync script

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -11,7 +11,6 @@ sync() {
 repo init --depth=1 --no-repo-verify -u $ROMREPO
 git clone $LOCALMANIFEST .repo/local_manifests
 repo sync --current-branch --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$( nproc --all )
-rm -rf hardware/qcom-caf/sm8250
 }
 echo "Syncing Rom & Device Sources"
 sync
