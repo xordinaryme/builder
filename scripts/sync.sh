85405a66da022e6428f9045c3f565ab83aea5a36
Liam <162432464+belowzeroiq@users.noreply.github.com>
2025-03-28 02:52:30 +0700

    Fix sync.sh to remove quotes around LOCALMANIFEST in git clone command

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -11,7 +11,7 @@ chk
 
 sync_repo() {
     repo init --depth=1 --no-repo-verify -u $ROMREPO
-    git clone "$LOCALMANIFEST" .repo/local_manifests
+    git clone $LOCALMANIFEST .repo/local_manifests
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 }
 
