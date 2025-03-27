4c92b03cd5e3db4ff3f181dbeeccf6f80351005f
Liam <162432464+belowzeroiq@users.noreply.github.com>
2025-03-27 22:37:14 +0700

    Refactor sync function in sync.sh to improve error handling and clarity

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,16 +1,24 @@
 #!/bin/bash
+
 chk() {
-df -h
-free -h
-lscpu
+    df -h
+    free -h
+    lscpu
 }
+
 echo "Checking System Configuration"
 chk
 
-sync() {
-repo init --depth=1 --no-repo-verify -u $ROMREPO
-git clone $LOCALMANIFEST .repo/local_manifests
-repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$( nproc --all )
+sync_repo() {
+    if [[ -z "$ROMREPO" || -z "$LOCALMANIFEST" ]]; then
+        echo "Error: ROMREPO or LOCALMANIFEST is not set."
+        exit 1
+    fi
+
+    repo init --depth=1 --no-repo-verify -u "$ROMREPO"
+    git clone "$LOCALMANIFEST" .repo/local_manifests
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 }
-echo "Syncing Rom & Device Sources"
-sync
+
+echo "Syncing ROM & Device Sources"
+sync_repo
