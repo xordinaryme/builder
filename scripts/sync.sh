b9da9190cf9140318241c4b8035d019117bfcc19
Liam <162432464+belowzeroiq@users.noreply.github.com>
2025-03-27 22:29:48 +0700

    Refactor sync command in sync.sh to use shorthand for current branch

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -10,7 +10,7 @@ chk
 sync() {
 repo init --depth=1 --no-repo-verify -u $ROMREPO
 git clone $LOCALMANIFEST .repo/local_manifests
-repo sync --current-branch --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$( nproc --all )
+repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$( nproc --all )
 }
 echo "Syncing Rom & Device Sources"
 sync
