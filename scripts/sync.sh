dfd6b8a0c5684c9b64dcdf6871e012f3ebc32b7c
Liam <162432464+belowzeroiq@users.noreply.github.com>
2025-03-27 22:43:27 +0700

    Simplify sync_repo function by removing redundant error checks for ROMREPO and LOCALMANIFEST

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -10,12 +10,7 @@ echo "Checking System Configuration"
 chk
 
 sync_repo() {
-    if [[ -z "$ROMREPO" || -z "$LOCALMANIFEST" ]]; then
-        echo "Error: ROMREPO or LOCALMANIFEST is not set."
-        exit 1
-    fi
-
-    repo init --depth=1 --no-repo-verify -u "$ROMREPO"
+    repo init --depth=1 --no-repo-verify -u $ROMREPO
     git clone "$LOCALMANIFEST" .repo/local_manifests
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 }
