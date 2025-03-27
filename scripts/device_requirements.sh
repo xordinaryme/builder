66aa203451e200a8e7ba78db0d6f4defb7e890c8
Liam <162432464+belowzeroiq@users.noreply.github.com>
2025-03-28 02:36:55 +0700

    Comment out display hardware removal and cloning commands in device_requirements.sh

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -4,7 +4,7 @@ set_requirements() {
 # remove source prebuilt
 # rm -rf external/tinycompress
 # rm -rf hardware/qcom/audio
-rm -rf hardware/qcom/display
+# rm -rf hardware/qcom/display
 # rm -rf hardware/qcom/media
 # rm -rf vendor/qcom/opensource/arpal
 # rm -rf vendor/qcom/opensource/agm
@@ -13,7 +13,7 @@ rm -rf vendor/lineage
 # clone requirements for device
 # git clone --depth=1 https://github.com/Xiaomi-SD685-Devs/external_tinycompress -b 13 external/tinycompress
 # git clone --depth=1 https://github.com/Xiaomi-SD685-Devs/hardware_qcom_audio -b lineage-20.0-caf-sm6225 hardware/qcom/audio
-git clone --depth=1 https://github.com/Xiaomi-SD685-Devs/hardware_qcom_display -b lineage-20.0-caf-sm6225 hardware/qcom/display
+# git clone --depth=1 https://github.com/Xiaomi-SD685-Devs/hardware_qcom_display -b lineage-20.0-caf-sm6225 hardware/qcom/display
 # git clone --depth=1 https://github.com/Xiaomi-SD685-Devs/hardware_qcom_media -b lineage-20.0-caf-sm6225 hardware/qcom/media
 # git clone --depth=1 https://github.com/Xiaomi-SD685-Devs/vendor_qcom_opensource_arpal -b lineage-20.0-caf-sm6225 vendor/qcom/opensource/arpal
 # git clone --depth=1 https://github.com/Xiaomi-SD685-Devs/vendor_qcom_opensource_agm -b lineage-20.0-caf-sm6225 vendor/qcom/opensource/agm
