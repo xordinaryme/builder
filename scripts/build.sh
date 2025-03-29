848b75a34e2861fdbad5dec0e5adb0d32ec779e9
belowzeroiq <belowzeroiq@proton.me>
2025-03-29 19:05:01 +0700

    Remove unused environment variable exports in build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -76,10 +76,6 @@ upload_ccache() {
 build() {
   source build/envsetup.sh || . build/envsetup.sh
   lunch $MAKEFILENAME-$VARIENT
-  #export SKIP_ABI_CHECKS=true
-  #export SKIP_API_CHECKS=true
-  #export ALLOW_MISSING_DEPENDENCIES=true
-  #export PRODUCT_OTA_ENFORCE_VINTF_KERNEL_REQUIREMENTS=false
   $EXTRACMD
   $TARGET -j$(nproc --all) & sleep 95m
 }
