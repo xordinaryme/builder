9af2904750176d7acdaec89c8e4db064ed0bc618
belowzeroiq <belowzeroiq@proton.me>
2025-03-29 19:36:05 +0700

    Remove local_manifests directory before cloning in sync_repo function

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -11,6 +11,7 @@ chk
 
 sync_repo() {
     repo init --depth=1 --no-repo-verify -u $ROMREPO
+    rm -rf .repo/local_manifests
     git clone $LOCALMANIFEST .repo/local_manifests
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 }
