c21368e5cab9e509296315706f34bcf5b0fdc3bf
belowzeroiq <belowzeroiq@proton.me>
2025-04-01 17:03:26 +0700

    Fix sync script to use all available CPU cores for repo sync

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -13,7 +13,7 @@ sync_repo() {
     repo init --depth=1 --no-repo-verify -u $ROMREPO
     rm -rf .repo/local_manifests
     git clone $LOCALMANIFEST .repo/local_manifests
-    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(( $(nproc --all) - 1 ))
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 }
 
 echo "Syncing ROM & Device Sources"
