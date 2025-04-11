fc9cffd3feaae48cd0691c2b5e8187323c4fab1c
Liam <162432464+belowzeroiq@users.noreply.github.com>
2025-04-11 04:59:28 -0700

    build: update device requirements for ArrowOS and QCOM display

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 set_requirements() {
-rm -rf vendor/lineage
-git clone --depth=1 https://github.com/passive-development/vendor_lineage -b lineage-20.0 vendor/lineage
+rm -rf vendor/arrow
+rm -rf vendor/qcom/opensource/display
+git clone --depth=1 https://github.com/passive-development/android_vendor_arrow -b arrow-13.1 vendor/arrow
+git clone --depth=1 https://github.com/passive-development/android_vendor_qcom_opensource_display -b lineage-20.0 vendor/qcom/opensource/display
 }
 
 set_requirements
