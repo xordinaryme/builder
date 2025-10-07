5c93fd6a58afb15d864fd70583324fb49ac45723
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-08 01:00:19 +0700

    Update device_requirements.sh

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-rm -rf vendor/crdroid
-git clone --depth=1 https://github.com/ordinary-topaz-lab/android_vendor_crdroid -b 13.0 vendor/crdroid
-# rm -rf hardware/xiaomi
-# git clone --depth=1 https://github.com/ordinary-topaz-lab/hardware_xiaomi -b 13 hardware/xiaomi
+rm -rf vendor/aosp
+git clone --depth=1 https://github.com/ordinary-topaz-lab/android_vendor_aosp_legacy -b 13 vendor/aosp
+rm -rf hardware/xiaomi
+git clone --depth=1 https://github.com/ordinary-topaz-lab/hardware_xiaomi -b 13 hardware/xiaomi
 
 echo "Device requirements set successfully."
