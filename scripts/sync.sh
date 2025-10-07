5187743b9582043c372f3735af73896fdb5e2c3a
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-07 22:10:22 +0700

    project infinity

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sync_repo() {
-    repo init --depth=1 --no-repo-verify -u $ROMREPO --git-lfs
+    repo init --depth=1 --no-repo-verify --git-lfs -u $ROMREPO -g default,-mips,-darwin,-notdefault
     rm -rf .repo/local_manifests
     git clone $LOCALMANIFEST .repo/local_manifests
-    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
+    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j$(nproc --all)
 }
 
 echo "Syncing ROM & Device Sources"
