63658b2a9f1c7aa9395e3377bcec0175229a9aa6
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-08 01:18:12 +0700

    Update sync.sh

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sync_repo() {
-    repo init --depth=1 --no-repo-verify -u $ROMREPO --git-lfs
+    repo init --depth=1 --no-repo-verify -u $ROMREPO
     rm -rf .repo/local_manifests
     git clone $LOCALMANIFEST .repo/local_manifests
     repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
