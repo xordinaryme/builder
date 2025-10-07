8c01d2da5b99d88ecd759b2d7e46f31d063ec1b9
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-07 19:56:21 +0700

    simplified device requirements

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
 
-set_requirements() {
 rm -rf vendor/aosp
 git clone --depth=1 https://github.com/ordinary-topaz-lab/vendor_aosp_legacy -b thirteen vendor/aosp
-}
+rm -rf hardware/xiaomi
+git clone --depth=1 https://github.com/ordinary-topaz-lab/hardware_xiaomi -b 13 hardware/xiaomi
 
-set_requirements
 echo "Device requirements set successfully."
