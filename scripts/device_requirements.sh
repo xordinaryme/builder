c2a3c4e45e9c076f481d71477a2854a188f6a2a7
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-08 01:23:36 +0700

    Update device_requirements.sh

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 rm -rf vendor/aosp
-git clone --depth=1 https://github.com/ordinary-topaz-lab/android_vendor_aosp_legacy -b 13 vendor/aosp
+git clone --depth=1 https://github.com/ordinary-topaz-lab/android_vendor_aosp_legacy -b thirteen-plus vendor/aosp
 rm -rf hardware/xiaomi
 git clone --depth=1 https://github.com/ordinary-topaz-lab/hardware_xiaomi -b 13 hardware/xiaomi
 
