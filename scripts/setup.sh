40797ce0da861288ad3e674fb5e6860672376d7e
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-08 12:29:22 +0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,7 +4,7 @@ sudo apt update && sudo apt install -y \
   bc bison build-essential ccache curl \
   flex g++-multilib gcc-multilib git git-lfs \
   gnupg gperf imagemagick lib32ncurses-dev lib32readline-dev \
-  lib32z1-dev liblz4-tool libncurses6 libncurses-dev \
+  lib32z1-dev liblz4-tool libncurses5 libncurses6 libncurses-dev \
   libsdl1.2-dev libssl-dev libwxgtk3.2-dev libxml2 libxml2-utils \
   lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev \
   git repo
