c2b54b58293a7c587ed5d4a650c7097bd7911e6e
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-08 12:08:47 +0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install -y \
-    bc bison build-essential ccache curl flex \
-    g++-multilib gcc-multilib git gnupg gperf \
-    lib32readline-dev lib32z1-dev \
-    liblz4-tool libncurses5-dev libsdl1.2-dev \
-    libncurses5:i386 lib32ncurses6 \
-    libssl-dev libxml2 libxml2-utils \
-    zstd pigz lzop \
-    rsync schedtool squashfs-tools zip \
-    android-tools-adb android-tools-fastboot \
-    python-is-python3 python3 python3-pip jq \
-    repo git \
-    git \
-    git-lfs \
-    android-sdk-platform-tools-common
+sudo apt update && sudo apt install \
+ bc bison build-essential ccache curl \
+ flex g++-multilib gcc-multilib git git-lfs \
+ gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev \
+ lib32z1-dev liblz4-tool libncurses5 libncurses5-dev \
+ libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils \
+ lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 
 sudo apt upgrade -y
     
