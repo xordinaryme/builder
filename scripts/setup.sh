f6a942a2558abf2fc0a6bf214f775c6258a286b5
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-08 12:12:42 +0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-sudo apt update && sudo apt install \
- bc bison build-essential ccache curl \
- flex g++-multilib gcc-multilib git git-lfs \
- gnupg gperf imagemagick lib32ncurses5-dev lib32readline-dev \
- lib32z1-dev liblz4-tool libncurses5 libncurses5-dev \
- libsdl1.2-dev libssl-dev libwxgtk3.0-gtk3-dev libxml2 libxml2-utils \
- lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
+sudo apt update && sudo apt install -y \
+  bc bison build-essential ccache curl \
+  flex g++-multilib gcc-multilib git git-lfs \
+  gnupg gperf imagemagick lib32ncurses-dev lib32readline-dev \
+  lib32z1-dev liblz4-tool libncurses6 libncurses-dev \
+  libsdl1.2-dev libssl-dev libwxgtk3.2-dev libxml2 libxml2-utils \
+  lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev
 
 sudo apt upgrade -y
-    
+  
 echo "Setting Up AOSP Build Environment"
