f3d23d4306faa112cca260a2ed385c24d33cef48
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-08 12:39:47 +0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -11,8 +11,7 @@ sudo apt update && sudo apt install -y \
 
 sudo apt upgrade -y
 
-wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_amd64.deb && sudo dpkg -i libtinfo5_6.4-2_amd64.deb && rm -f libtinfo5_6.4-2_amd64.deb
-wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.4-2_amd64.deb && sudo dpkg -i libncurses5_6.4-2_amd64.deb && rm -f libncurses5_6.4-2_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.2-0ubuntu2_amd64.deb && sudo dpkg -i libncurses5_6.2-0ubuntu2_amd64.deb && rm -rf libncurses5_6.2-0ubuntu2_amd64.deb
 
 sudo apt install lib32ncurses5-dev libncurses5 libncurses5-dev -y 
   
