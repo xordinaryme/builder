4e13c4e1aadf774642c4a6f5e7e3c2c69a7adeaf
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-08 22:25:39 +0700

    Update device_requirements.sh

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -2,6 +2,6 @@
 
 
 rm -rf vendor/lineage
-git clone --depth=1 https://github.com/ordinary-topaz-lab/vendor_lineage -b thirteen-plus vendor/lineage
+git clone --depth=1 https://github.com/ordinary-topaz-lab/android_vendor_lineage -b lineage-20.0 vendor/lineage
 
 echo "Device requirements set successfully."
