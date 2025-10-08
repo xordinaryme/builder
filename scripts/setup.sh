e42d5f0a3ecab6b0f8ed392824da58dda4bf1449
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-08 12:32:15 +0700

    Update setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -4,11 +4,16 @@ sudo apt update && sudo apt install -y \
   bc bison build-essential ccache curl \
   flex g++-multilib gcc-multilib git git-lfs \
   gnupg gperf imagemagick lib32ncurses-dev lib32readline-dev \
-  lib32z1-dev liblz4-tool libncurses5 libncurses6 libncurses-dev \
+  lib32z1-dev liblz4-tool libncurses6 libncurses-dev \
   libsdl1.2-dev libssl-dev libwxgtk3.2-dev libxml2 libxml2-utils \
   lzop pngcrush rsync schedtool squashfs-tools xsltproc zip zlib1g-dev \
-  git repo
+  git repo wget
 
 sudo apt upgrade -y
+
+wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libtinfo5_6.4-2_amd64.deb && sudo dpkg -i libtinfo5_6.4-2_amd64.deb && rm -f libtinfo5_6.4-2_amd64.deb
+wget http://archive.ubuntu.com/ubuntu/pool/universe/n/ncurses/libncurses5_6.4-2_amd64.deb && sudo dpkg -i libncurses5_6.4-2_amd64.deb && rm -f libncurses5_6.4-2_amd64.deb
+
+sudo apt install lib32ncurses5-dev libncurses5 libncurses5-dev -y 
   
 echo "Setting Up AOSP Build Environment"
