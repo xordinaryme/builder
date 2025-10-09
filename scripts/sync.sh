a9b29b9857e4bd9feadb68169e3017ba7b0758f3
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-09 23:43:23 +0700

    update

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 sync_repo() {
-    repo init --depth=1 --no-repo-verify -u $ROMREPO --git-lfs
+    repo init -u $ROMREPO --no-repo-verify --depth=1 --git-lfs
     rm -rf .repo/local_manifests
     git clone $LOCALMANIFEST .repo/local_manifests
-    repo sync -c --no-clone-bundle --no-tags --optimized-fetch --prune --force-sync -j10
+    repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
 }
 
 echo "Syncing ROM & Device Sources"
