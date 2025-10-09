78d0146e1213eba14bbb38775e4329ffb7e58389
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-10 00:24:23 +0700

    Update device_requirements.sh

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -2,6 +2,6 @@
 
 
 rm -rf hardware/qcom-caf/common
-git clone --depth=1 https://github.com/xordinaryme/android_hardware_qcom-caf_common -b lineage-23.0 hardware/qcom--caf/common
+git clone --depth=1 https://github.com/xordinaryme/android_hardware_qcom-caf_common -b lineage-23.0 hardware/qcom-caf/common
 
 echo "Device requirements set successfully."
