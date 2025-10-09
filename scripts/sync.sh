3601f1d0e54d116583b6108d7ddad0a2de373a81
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-10-09 23:55:07 +0700

    update

--- scripts/sync.sh
+++ scripts/sync.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 sync_repo() {
-    repo init -u https://github.com/AndroidOne-Experience/manifest.git -b 15 --no-repo-verify --depth=1 --git-lfs
+    repo init -u $ROMREPO --no-repo-verify --depth=1 --git-lfs
     rm -rf .repo/local_manifests
     git clone $LOCALMANIFEST .repo/local_manifests
     repo sync -c -j8 --force-sync --no-clone-bundle --no-tags
