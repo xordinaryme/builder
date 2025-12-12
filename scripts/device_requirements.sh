999f922607e0ecb160a68dc64540f60f9231e03f
Iqbal <224786693+xordinaryme@users.noreply.github.com>
2025-12-12 07:42:11 +0700

    Update device_requirements.sh

--- scripts/device_requirements.sh
+++ scripts/device_requirements.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 
-rm -rf hardware/xiaomi
-git clone --depth=1 https://github.com/xordinaryme/hardware_xiaomi -b thirteen hardware/xiaomi
+# rm -rf hardware/xiaomi
+# git clone --depth=1 https://github.com/xordinaryme/hardware_xiaomi -b thirteen hardware/xiaomi
 
-rm -rf vendor/aosp
-git clone --depth=1 https://github.com/xordinaryme/vendor_aosp -b thirteen-plus vendor/aosp
+rm -rf vendor/lineage
+git clone --depth=1 https://github.com/topnotchfreaks/android_vendor_lineage -b lineage-20.0 vendor/lineage
 
 echo "Device requirements set successfully."
